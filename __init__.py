5a6ece87fd38012f65cbbb4f80733b5827669353
blissland <blissland.house@googlemail.com>
2015-03-19 09:09:40 +0000

    Just better

--- __init__.py
+++ __init__.py
@@ -1,12 +1,15 @@
 import chanutils.torrent
-from chanutils import get_doc, post_doc, select_all, select_one, get_attr
+from chanutils import select_all, select_one, get_attr
+from chanutils import get_doc, post_doc, get_json
 from chanutils import get_text, get_text_content, replace_entity, byte_size
-from playitem import TorrentPlayItem, PlayItemList
+from playitem import TorrentPlayItem, ShowMoreItem, PlayItemList
 
-_SEARCH_URL = "https://eztv.ch/search/"
+#_SEARCH_URL = "https://eztv.ch/search/"
+_SEARCH_URL = "http://eztvapi.re/shows/1?keywords="
 
 _FEEDLIST = [
   {'title':'Latest', 'url':'https://eztv.ch'},
+  {'title':'Popular', 'url':'http://eztvapi.re/shows/1'},
 ]
 
 def name():
@@ -22,15 +25,23 @@ def feedlist():
   return _FEEDLIST
 
 def feed(idx):
-  doc = get_doc(_FEEDLIST[idx]['url'], proxy=True)
-  return _extract(doc)
+  if idx > 0:
+    data = get_json(_FEEDLIST[idx]['url'])
+    return _extract_showlist(data)
+  else:
+    doc = get_doc(_FEEDLIST[idx]['url'], proxy=True)
+    return _extract_html(doc)
 
 def search(q):
-  payload = {'SearchString1':q, 'SearchString':'', 'search':'Search'}
-  doc = post_doc(_SEARCH_URL, payload)
-  return _extract(doc)
+  url = _SEARCH_URL + q
+  data = get_json(url)
+  return _extract_showlist(data)
 
-def _extract(doc):
+def showmore(imdb_id):
+  data = get_json('http://eztvapi.re/show/' + imdb_id)
+  return _extract_show(data)
+
+def _extract_html(doc):
   rtree = select_all(doc, 'tr.forum_header_border')
   results = PlayItemList()
   for l in rtree:
@@ -40,4 +51,29 @@ def _extract(doc):
     el = select_one(l, 'a.magnet')
     url = get_attr(el, 'href')
     results.add(TorrentPlayItem(title, img, url))
+  return results
+
+def _extract_showlist(data):
+  results = PlayItemList()
+  for r in data:
+    title = r['title']
+    img = r['images']['poster']
+    url = r['imdb_id']
+    subtitle = "Year: " + r['year'] + ", "
+    subtitle = subtitle + "Seasons: " + str(r['num_seasons']) + ", "
+    subtitle = subtitle + "Rating: " + str(r['rating']['percentage']) + "%"
+    synopsis = '<a target="_blank" href="http://www.imdb.com/title/' + r['imdb_id'] + '/">View on IMDB</a>'
+    results.add(ShowMoreItem(title, img, url, subtitle, synopsis))
+  return results
+
+def _extract_show(data):
+  results = PlayItemList()
+  img = data['images']['poster']
+  rtree = data['episodes'] 
+  for r in rtree:
+    title = r['title']
+    url = r['torrents']['0']['url']
+    subtitle = "Season: " + str(r['season']) + ", Episode: " + str(r['episode'])
+    synopsis = r['overview']
+    results.add(TorrentPlayItem(title, img, url, subtitle, synopsis))
   return results
